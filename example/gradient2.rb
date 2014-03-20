3874138b2e752fcc1280cea161d2440a7fb4210c
Sophy Eung <ungsophy@gmail.com>
2014-03-20 23:25:36 +0700

    Added more examples

--- example/gradient2.rb
+++ example/gradient2.rb
@@ -2,5 +2,4 @@ require './lib/rainbow'
 require_relative 'gradient_factory'
 
 filenames = GradientFactory.create_from_file('gradient2.yml')
-
 `open #{filenames.join(' ')}`
--- example/gradient3.rb
+++ example/gradient3.rb
@@ -1,18 +1,5 @@
 require './lib/rainbow'
+require_relative 'gradient_factory'
 
-first_color  = Rainbow::Color.new(ChunkyPNG::Color(0, 0, 255), 20)
-second_color = Rainbow::Color.new(ChunkyPNG::Color(255, 0, 0), 50)
-third_color  = Rainbow::Color.new(ChunkyPNG::Color(0, 255, 0), 100)
-color_range1 = Rainbow::ColorRange.new(first_color, second_color, 50)
-color_range2 = Rainbow::ColorRange.new(second_color, third_color, 50)
-
-first_opacity  = Rainbow::Opacity.new(100, 10)
-second_opacity = Rainbow::Opacity.new(40, 50)
-third_opacity  = Rainbow::Opacity.new(100, 100)
-opacity_range1 = Rainbow::OpacityRange.new(first_opacity, second_opacity, 50)
-opacity_range2 = Rainbow::OpacityRange.new(second_opacity, third_opacity, 50)
-
-filename = './example/output/gradient3.png'
-gradient = Rainbow::Gradient.new(400, 50, [color_range1, color_range2], [opacity_range1, opacity_range2])
-gradient.save_as_png(filename)
-`open #{filename}`
+filenames = GradientFactory.create_from_file('gradient3.yml')
+`open #{filenames.join(' ')}`
